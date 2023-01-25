04bcb3eda94352187dafac2129c869baca1d35b7
Akanshakansal01 <akanshakansal025@gmail.com>
2023-01-25 13:49:49 +0530

    newwww

--- NodeJS/FileSystem/Files/Template/app.js
+++ NodeJS/FileSystem/Files/Template/app.js
@@ -1,11 +1,25 @@
 const express=require("express");
 const app=express();
 const path=require("path");
+
 app.set('view engine','ejs');
 
+const todos=[
+    "playing football",
+    "reading books",
+    "cooking ",
+    "classical dance"
+]
 app.get('/home',(req,res)=>{
-    res.render('index')
+    const randomNumber=Math.floor(Math.random()*10);
+    res.render('index',{randomNumber})
 })
+
+app.get('/todos',(req,res)=>{
+    res.render('todos',{todos})
+})
+
+app.use(express.static(path.join(__dirname,'public')))
 
 app.set('views',path.join(__dirname,'views'))
 
